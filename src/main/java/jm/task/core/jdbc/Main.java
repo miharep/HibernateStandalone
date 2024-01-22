43ae24d99b1a425bcd4dd151a9bc737acc47b0e9
Михаил <mih.borey@gmail.com>
2024-01-22 19:42:05 +0500

    clean up code from previous task (work with database by JDBC)

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,46 +11,18 @@ public class Main {
         
         UserService userService = new UserServiceImpl();
         
-        //Создание таблицы User(ов)
-//        userService.createUsersTable();
-        
-        //Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
-//        userService.saveUser("Иван", "Иванов" , (byte) 11);
-//        userService.saveUser("Петр", "Петров" , (byte) 22);
-//        userService.saveUser("Сидор", "Сидоров" , (byte) 33);
-//        userService.saveUser("Четвертый", "Чел" , (byte) 44);
-        
-        //Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
-//        System.out.println(User.toString(userService.getAllUsers()));
-        
-        //Очистка таблицы User(ов)
-//        userService.cleanUsersTable();
-        
-        //Удаление таблицы
-//        userService.dropUsersTable();
-        
-        
-        //========== Hibernate ==========
-        
         try (SessionFactory sessionFactory = Util.getSessionFactory()) {
-            
-            //Создание таблицы User(ов)
             userService.createUsersTable();
             
-            //Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
             userService.saveUser("Иван", "Иванов" , (byte) 11);
             userService.saveUser("Петр", "Петров" , (byte) 22);
             userService.saveUser("Сидор", "Сидоров" , (byte) 33);
             userService.saveUser("Еще", "Tot" , (byte) 22);
-            
-            //Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
+
             System.out.println(User.toString(userService.getAllUsers()));
-            
-            //Очистка таблицы User(ов)
-            //userService.removeUserById(1);
+
             userService.cleanUsersTable();
             
-            //Удаление таблицы
             userService.dropUsersTable();
         }
     }
