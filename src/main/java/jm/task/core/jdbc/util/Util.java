f6583f01f7eb5547ce800521b7438075ce1cd06a
Михаил <mih.borey@gmail.com>
2024-01-22 16:55:35 +0500

    add SessionFactory creation

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,9 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,8 +13,18 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mysql-kata";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
+    
+    private static SessionFactory sessionFactory = null;
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            sessionFactory = new Configuration().addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+        }
+        return sessionFactory;
     }
 }
