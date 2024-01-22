fdddb35c9faaa3710faa788db1c015c164b40f6c
Михаил <mih.borey@gmail.com>
2024-01-22 19:35:39 +0500

    add try-catch for Util.getSessionFactory()

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,29 +1,26 @@
 package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Util {
-    // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:mysql://localhost:3306/mysql-kata";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
-    
-    private static SessionFactory sessionFactory = null;
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
+    private static SessionFactory sessionFactory = null;
 
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
-            sessionFactory = new Configuration().addAnnotatedClass(User.class)
-                    .buildSessionFactory();
+            try {
+                sessionFactory = new Configuration()
+                        .addAnnotatedClass(User.class)
+                        .buildSessionFactory();
+            } catch (HibernateException e) {
+                e.printStackTrace();
+                if (sessionFactory != null) {
+                    sessionFactory.close();
+                }
+            }
         }
         return sessionFactory;
     }
